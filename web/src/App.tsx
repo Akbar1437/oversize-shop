85be2453c285c162f2988b35bb7507c9a2d09927
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2023-04-29 11:28:26 +0500

    added list of products

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,25 @@
+import { sampleProducts } from "./data";
+
 function App() {
   return (
     <div>
       <header>TS Oversize</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product, index) => (
+            <li key={index}>
+              <img
+                src={product.image}
+                alt={product.name}
+                width={400}
+                height={400}
+              />
+              <h2>{product.name}</h2>
+              <p>{product.price}$</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   );
