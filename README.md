ce87e857fd7c400f97ea3e1dc933f159682fbdfe
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2023-04-29 10:46:02 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OVERSIZE shop
---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header>TS Oversize</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
