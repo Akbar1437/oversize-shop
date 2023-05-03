d28ec062cc5056253d484ed77e05a1b26ed47f0b
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2023-05-03 06:49:03 +0500

    working on cart page product

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -15,12 +15,14 @@ import ProductPage from "./pages/ProductPage.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StoreProvider } from "./Store.tsx";
+import CartPage from "./pages/CartPage.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomePage />} />
       <Route path="product/:slug" element={<ProductPage />} />
+      <Route path="cart" element={<CartPage />} />
     </Route>
   )
 );
